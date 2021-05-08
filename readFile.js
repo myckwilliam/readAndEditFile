96bf26bd34369ce09b6b9b0897c5121cca4d9c3a
Myck William dos Santos <myckwilliam@gmail.com>
2021-05-08 14:14:41 -0300

    Create readFile.js

---
+++ readFile.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const path = require('path')
+
+const caminho = path.join(__dirname, 'nomes.txt')
+//Callback
+const toStringSplit = (conteudo) => {
+    const string = conteudo.toString();
+    const arr = string.split(' ');
+    return arr
+}
+
+const exibirConteudo = (_, conteudo) => {
+    console.log(conteudo.toString());
+    console.log(" ");
+}
+
+const exibirComQuebraDeLinha = (_, conteudo) => {
+    arr = toStringSplit(conteudo);
+    arr.map(element => console.log(element));
+    console.log(" ");
+}
+
+const exibirEmOrdemAlfabetica = (_, conteudo) => {
+    const arrSort = toStringSplit(conteudo).sort();
+    arrSort.map(element => console.log(element));
+    console.log(' ')
+}
+
+fs.readFile(caminho, exibirConteudo);
+fs.readFile(caminho, exibirComQuebraDeLinha)
+fs.readFile(caminho, exibirEmOrdemAlfabetica)
+
+console.log("Vai aparecer primeiro.")
+console.log(' ');

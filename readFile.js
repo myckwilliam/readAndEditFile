ff9f1456e45658c0a247acf01f15488899ea8960
Myck Santos <myckwilliam@gmail.com>
2021-05-18 05:55:23 -0300

    Update readFile.js

--- readFile.js
+++ readFile.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const { promisify } = require('util');
 
 const listaPrincipal = [];
 const palavrasUsadas = [];
@@ -10,7 +11,7 @@ const transformarEmString = (string) => {
 }
 
 const removerTodasSujeiras = (string) => {
-    return string.replace(/([\u0300-\u036f]|[^a-zA-Z' 'à-úÀ-Ú])/g, '');
+    return string.replace(/([^a-zA-Z' 'à-úÀ-Ú])/g, '');
 }
 
 const separarPalavras = (string) => {
@@ -23,7 +24,7 @@ const retirarEspacosVazios = (lista) => {
 
 const adicionarAListaPrincipal = (lista) => {
     lista.map(element => listaPrincipal.push(element));
-    return listaPrincipal
+    return (listaPrincipal)
 }
 
 const criarObjetoContagem = (palavra, quantidade, percentual) => {
@@ -40,12 +41,34 @@ const obterInfoDasPalavras = (listaPalavras) => {
             palavrasUsadas.push(element);
             const listaQuantidade = listaPalavras.filter(element2 => element2 === element);
             const quantidade = listaQuantidade.length;
-            const percentual = (quantidade/listaPalavras.length) * 100;
+            const percentual = ((quantidade/listaPalavras.length) * 100).toFixed(2);
             listaObjetos.push(criarObjetoContagem(element, quantidade, percentual))
         }   
     })
     return listaObjetos
 }
+
+const ordenaListaDeAcordoComAQuantidade = lista => {
+    lista.sort( (a, b) => {
+        if (a.quantidade > b.quantidade) {
+          return -1;
+        }
+        if (a.quantidade < b.quantidade) {
+          return 1;
+        }
+        return 0;
+      });
+    return lista;
+}
+
+const transformarEmJSON = lista => {
+    return JSON.stringify(lista, null, ' ')
+}
+
+const escreverArquivo = JSON => {
+    fs.writeFileSync(path.join(__dirname, 'teste.txt'), JSON)
+}
+
 
 const composicao = (...fns) => (_, lista) => fns.reduce((acc,fn) => fn(acc), lista)
 
@@ -55,17 +78,32 @@ const tratamentoDoArquivo = composicao(
     separarPalavras,
     retirarEspacosVazios,
     adicionarAListaPrincipal,
-    obterInfoDasPalavras
+   
+)
+const tratamentoDoArquivo2 = composicao(
+    transformarEmString,
+    removerTodasSujeiras,
+    separarPalavras,
+    retirarEspacosVazios,
+    adicionarAListaPrincipal,
+    obterInfoDasPalavras,
+    ordenaListaDeAcordoComAQuantidade,
+    transformarEmJSON,
+    escreverArquivo
+   
 )
 
 const gerarResultado = (dados) => {
-    dados.map(element => fs.readFile(path.join(__dirname, element), tratamentoDoArquivo));
+    dados.map(element => {
+        dados.indexOf(element) === (dados.length - 1) 
+        ? fs.readFile(path.join(__dirname, element), tratamentoDoArquivo2)
+        : fs.readFile(path.join(__dirname, element), tratamentoDoArquivo)
+    })
 }
 
-const dados = ['nomes.txt', 'nomes2.txt']
+const dados = ['dados1.txt', 'dados2.txt', 'dados3.txt'];
 
-console.log(gerarResultado(dados))
-
+gerarResultado(dados)
 
 
 
